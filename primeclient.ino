2e2f130966f22ad340cdbc9cebfaa95c351593ed
Huy <duchuykun@gmail.com>
2014-06-08 13:16:04 +0700

    add polling

--- primeclient.ino
+++ primeclient.ino
@@ -1,14 +1,16 @@
 #include <SPI.h>
 #include <WiFi.h>
+#include "WebSocketClient.h"
 
-char ssid[] = "G2"; 
-char pass[] = "cubin123";
+char ssid[] = "LOZI";
+char pass[] = "kjnhkHungvl1";
 int keyIndex = 0;
 
 int status = WL_IDLE_STATUS;
-char server[] = "128.199.223.211";
+char server[] = "192.168.1.104";
 
 WiFiClient client;
+WebSocketClient webSocketClient;
 
 int speedValue = 2;
 
@@ -44,151 +46,130 @@ void setPwmFrequency(int pin, int divisor) {
 }
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    Serial.println("WiFi shield not present");
     while(true);
-  } 
+  }
 
   // attempt to connect to Wifi network:
-  while ( status != WL_CONNECTED) { 
+  while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
     delay(10000);
-  } 
+  }
   Serial.println("Connected to wifi");
   printWifiStatus();
 
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
-  if (connectToCenter(server, 3005)) {
-    //client.println("Hello sir!!!");
-  }
+  connectToCenter(server, 3005);
 }
 
 void loop() {
-  if (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-    if(c=='s' || c=='S'){
+  
+  String data;
+  if (client.connected()) {
+    webSocketClient.getData(data, NULL);
+    if (data.length() > 0) {
+      //Hao Nguyen TO DO
+      control(data);
+    } else {
+      polling();
+    }
+  } else {
+    Serial.println('Disconnect!');
+    connectToCenter(server, 3005);
+  }  
+  
+}
+
+void control(String signal)
+{
+   if(signal=="s" || signal=="s"){
       Serial.write("stop");
       analogWrite(3, 0);
       analogWrite(9, 0);
+      return;
     }
-    if(c=='f' || c=='F'){
+    if(signal=="f" || signal=="F"){
       Serial.write("go forward");
       analogWrite(3, 16*speedValue);
       analogWrite(9, 0);
+      return;
     }
-    if(c=='b' || c=='B'){
+    if(signal=="b" || signal=="B"){
       Serial.write("go backward");
       analogWrite(3, 0);
       analogWrite(9, 16*speedValue);
+      return;
     }
-    if(c=='r' || c=='R'){
+    if(signal=="r" || signal=="R"){
       Serial.write("turn right");
       analogWrite(5, 128);
       analogWrite(6, 128);
+      return;
     }
-    if(c=='l' || c=='L'){
+    if(signal=="l" || signal=="L"){
       Serial.write("turn left");
       analogWrite(5, 128);
       analogWrite(6, 0);
+      return;
     }
-    if(c=='s' || c=='S'){
+    if(signal=="s" || signal=="S"){
       Serial.write("stop");
       analogWrite(3, 0);
       analogWrite(9, 0);
+      return;
     }
-    if(c=='u' || c=='U'){
+    if(signal=="u" || signal=="U"){
       Serial.write("increase speed");
       speedValue++;
+      return;
     }
-    if(c=='d' || c=='D'){
+    if(signal=="d" || signal=="d"){
       Serial.write("decrease speed");
       speedValue--;
+      return;
     }
-  }
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-    connectToCenter(server, 3005);
-  }
+}
+
+void polling()
+{
+  webSocketClient.sendData("ping");
 }
 
 bool connectToCenter(char host[], int port)
 {
-  if (client.connect(server, 3005)) {
-    sendHandShake();
-    return readHandShake();
-  } 
-  else {
-    Serial.println("Connect fail :(");
+ if (client.connect("192.168.1.104", 3005)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
     return false;
-  } 
-}
-
-void poll()
-{
-
-}
-
-void sendHandShake()
-{
-  Serial.println("send handshake to server");
-  client.println("GET / HTTP/1.1");
-  client.println("Host: 128.199.223.211/");
-  client.println("Sec-WebSocket-Version: 13");
-  client.println("Upgrade: websocket");
-  client.println("Sec-WebSocket-Key: KCsYRvuDylR1T59hn5g/ig==");
-  client.println("Connection: Upgrade");
-  client.println("Origin: 128.199.223.211/");
-  client.println();
-}
-
-bool readHandShake()
-{
-  Serial.println("read handshake to server");
-  bool result = false;
-  char character;
-  String handshake = "", line;
-  int maxAttempts = 300, attempts = 0;
-  while(client.available() == 0 && attempts < maxAttempts) 
-  { 
-    delay(100); 
-    attempts++;
   }
-  while((line = readLine()) != "") {
-    handshake += line + '\n';
-  }
-  Serial.println(handshake);
-  result = handshake.indexOf("6XD14FnDL4E3zuGVM11QkbruQW8=") != -1;
-  if(!result) {
-         client.stop();
+
+  // Handshake with the server
+  webSocketClient.path = "/";
+  webSocketClient.host = "192.168.1.104";
+  
+  if (webSocketClient.handshake(client)) {
+    Serial.println("Handshake successful");
+    webSocketClient.sendData("iamrobot");
+  } else {
+    Serial.println("Handshake failed.");
+    return false;  
   }
   return true;
-}
-
-String readLine() {
-  String line = "";
-  char character;
-  while(client.available() > 0 && (character = client.read()) != '\n') {
-    if (character != '\r' && character != -1) {
-      line += character;
-    }
-  }
-  return line;
+  
 }
 
 void printWifiStatus() {
@@ -207,5 +188,4 @@ void printWifiStatus() {
   Serial.print(rssi);
   Serial.println(" dBm");
 }
-
 
